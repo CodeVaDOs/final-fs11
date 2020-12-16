b8cf866df64d7734c22cd250f063de41c3be142f
Vadym <tvadost2003@gmail.com>
2020-12-16 14:51:38 +0200

    modify appjs

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-
-const App  = ()=> {
-        return (<>START HERE KJHDKJS </>)
+const App = () => {
+  return (
+    <div>Hello world!</div>
+  )
 };
 export default App;
--- client/src/layout/Draft.js
+++ client/src/layout/Draft.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Draft =()=> {
+const Draft = () => {
 
 };
 export default Draft;
