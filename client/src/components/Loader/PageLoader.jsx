36351c56bb623e6ed03c4195ad49aacc565d79f4
Вадик Мельниченко <melnicenkovadik@gmail.com>
2021-01-03 13:16:46 +0300

    initial commit

--- client/src/components/Loader/PageLoader.jsx
+++ client/src/components/Loader/PageLoader.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import cx from "classnames";
-import { Spin } from "antd";
 
 import css from "./styles.module.scss";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 const PageLoader = ({ loaded = true, ...res }) => {
+  const [prog, setProg] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProg((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     loaded && (
-      <div {...res} className={cx("app-loader", css.apploader)}>
-        <Spin size="large"/>
+      <div {...res} className={cx("app-loader", css.loader, {
+        [css.loaderHide]: loaded,
+      })}>
+        <LinearProgress variant="determinate" value={prog}/>
       </div>
-    )
-  );
+    ));
 };
 
 export default PageLoader;
--- client/src/components/Loader/Preloader.jsx
+++ client/src/components/Loader/Preloader.jsx
@@ -1,17 +1,36 @@
-import React from "react";
 import cx from "classnames";
-import { Spin } from "antd";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 import css from "./styles.module.scss";
 
+
 const Preloader = ({ loaded }) => {
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div
-      className={cx("app-loader", css.loader, {
-        [css.loaderHide]: loaded,
-      })}
-    >
-      <Spin size="large"/>
+
+    <div className={cx("app-loader", css.loader, {
+      [css.loaderHide]: loaded,
+    })}>
+      <LinearProgress variant="determinate" value={progress}/>
     </div>
   );
 };
