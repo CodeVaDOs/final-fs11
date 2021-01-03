d3500050e9e057b12777ccac1d4c72b3d6bbb3f2
Вадик Мельниченко <melnicenkovadik@gmail.com>
2021-01-03 21:27:38 +0300

    fix sonar

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,4 +1,4 @@
-gitimport React from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
