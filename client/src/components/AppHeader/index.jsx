21428d5f5c8e98af08345438656d5b8b7efe6715
Вадик Мельниченко <melnicenkovadik@gmail.com>
2021-01-08 22:27:30 +0300

    open appBar

--- client/src/components/AppHeader/index.jsx
+++ client/src/components/AppHeader/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { IconButton, List, } from "@material-ui/core";
+import { CssBaseline, IconButton, List, } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { ExpandLess, ExpandMore } from "@material-ui/icons";
 import HomeWorkIcon from '@material-ui/icons/HomeWork';
@@ -9,40 +9,38 @@ import { routes } from "@utils/routes";
 import { Btn } from "./Button";
 import clsx from "clsx";
 
-export const drawerHeight = 190;
+export const drawerHeight = 150;
 const drawerNormalize = 10;
 
 
 const useStyles = makeStyles((theme) => ({
+  contentUp: {
+    height: `calc(100% - ${drawerHeight + 40}px)`,
+    flexGrow: 1,
+  },
   appBar: {
-    borderRadius: "15%",
-    height: drawerHeight,
+
+    border: "1px solid transparent",
+    borderRadius: "10% 10% 10% 10%",
+    height: "auto",
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["height", 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-  },
-  appBarShift: {
-    borderRadius: "15%",
 
-    marginTop: drawerHeight + drawerNormalize,
-    transition: theme.transitions.create(["height", 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
   },
   menuButton: {
     borderRadius: "50%",
     border: 'none',
     background: '#EEF5FF',
     position: "relative",
-    top: 0,
+    top: "10px",
     transform: "scale(1.2)",
     width: "25px",
     height: "25px",
     left: `calc(50%)`,
-    zIndex: 100
+    zIndex: 100,
   },
   list: {
     color: '#6E7375',
@@ -87,48 +85,63 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
     alignItems: 'center'
   },
+  toolbarUp: {},
+  toolbarDown: {},
 
 
 }));
-
+//
+// function HideOnScroll(props) {
+//   const { children, window } = props;
+//   const trigger = useScrollTrigger({ target: window ? window() : undefined });
+//
+//   return (
+//     <Slide appear={true} direction="down" in={!trigger}>
+//       {children}
+//     </Slide>
+//   );
+// }
 
 const AppHeader = (props) => {
   const classes = useStyles();
   const [open, setOpen] = useState(true);
-  // const [contentUp, setContentUpOpen] = useState(false);
-  //
-  // if (open) {
-  //   console.log('RFVJY');
-  //   setContentUpOpen(!contentUp);
-  //   dispach
-  // }
 
   return (
-    <div className={clsx(classes.appBar, {
-      [classes.appBarShift]: open,
-    })}>
-
-      <List className={classes.headerList}>
-        <div className={classes.logo}>
-          <div className={classes.logoW}>MARKSEM</div>
-          <div className={classes.logoCRM}> CRM</div>
-        </div>
-        <Btn icon={<HomeWorkIcon/>} title={'Home'} path={'/'}/>
-        <Btn icon={<SettingsIcon/>} title={'Settings'} path={'/settings'}/>
-        <Search/>
-      </List>
-      <div className={classes.menuLinks}>
-        <List className={classes.list}>
-          {routes.filter(r => !r.isHeaders).map((r, index) => (
-            <Btn key={index} icon={<r.icon/>} title={r.name} path={r.path}/>))}
+    <>
+      <div className={clsx(classes.appBar, {
+        [classes.toolbarUp]: open,
+        [classes.toolbarDown]: !open,
+      })}>
+        <List className={classes.headerList}>
+          <div className={classes.logo}>
+            <div className={classes.logoW}>MARKSEM</div>
+            <div className={classes.logoCRM}> CRM</div>
+          </div>
+          <Btn icon={<HomeWorkIcon/>} title={'Home'} path={'/'}/>
+          <Btn icon={<SettingsIcon/>} title={'Settings'} path={'/settings'}/>
+          <Search/>
         </List>
+        {open ?
+          // <HideOnScroll  {...props}>
+          //   <Box width="25%">
+          <div className={classes.menuLinks}>
+            <List className={classes.list}>
+              {routes.filter(r => !r.isHeaders).map((r, index) => (
+                <Btn key={index} icon={<r.icon/>} title={r.name} path={r.path}/>))}
+            </List>
+          </div>
+          //   </Box>
+          // </HideOnScroll>
+          : <CssBaseline/>
+        }
+        <div>
+          <IconButton onClick={() => setOpen(!open)} className={classes.menuButton}>
+            {open === true ? <ExpandLess/> : <ExpandMore/>}
+          </IconButton>
+        </div>
       </div>
-      <div>
-        <IconButton onClick={() => setOpen(!open)} className={classes.menuButton}>
-          {open === true ? <ExpandLess/> : <ExpandMore/>}
-        </IconButton>
-      </div>
-    </div>
+    </>
+
   );
 };
 
--- client/src/components/RightDrawer/index.js
+++ client/src/components/RightDrawer/index.js
@@ -3,13 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import IconButton from "@material-ui/core/IconButton";
 import { ChevronLeft, ChevronRight } from "@material-ui/icons";
-import { AppBar, CssBaseline, Drawer, Slide, useScrollTrigger, useTheme } from "@material-ui/core";
+import { AppBar, CssBaseline, Drawer } from "@material-ui/core";
 import ManagerCard from "./ManagerCard";
 import UserBar from "./UserBar";
 import AppHeader from "../AppHeader";
 import Divider from '@material-ui/core/Divider';
 import Container from "../Container";
-import Box from "@material-ui/core/Box";
 
 const drawerWidth = 440;
 
@@ -18,15 +17,12 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     justifyContent: 'flex-start',
-
   },
-
   appBar: {
     height: "auto",
     justifyContent: "flex-end",
     background: "#EEF5FF",
     boxSizing: 'border-box',
-    top: -175,
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', "height", 'margin'], {
       easing: theme.transitions.easing.sharp,
@@ -90,32 +86,16 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
+    padding: theme.spacing(12, 12),
     ...theme.mixins.toolbar,
   },
   content: {
     width: `calc(100% - ${drawerWidth + 40}px)`,
     flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  contentUpOpen: {
-    height: `calc(100% - ${drawerWidth + 40}px)`,
-    flexGrow: 1,
-    padding: theme.spacing(3),
   },
 
 }));
 
-function HideOnScroll(props) {
-  const { children, window } = props;
-  const trigger = useScrollTrigger({ target: window ? window() : undefined });
-
-  return (
-    <Slide appear={true} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
 
 const MiniDrawer = (props) => {
   const classes = useStyles();
@@ -128,17 +108,15 @@ const MiniDrawer = (props) => {
     <>
       <div className={classes.root}>
         <CssBaseline/>
-        <HideOnScroll  {...props}>
-          <Box width="25%">
-            <AppBar
-              className={clsx(classes.appBar,
-                {
-                  [classes.appBarShift]: open,
-                })}>
-              <AppHeader open={open} setOpen={setOpen}/>
-            </AppBar>
-          </Box>
-        </HideOnScroll>
+        <AppBar
+          className={clsx(classes.appBar,
+            {
+              [classes.appBarShift]: open,
+              [classes.toolbarUp]: open,
+              [classes.toolbarDown]: !open,
+            })}>
+          <AppHeader open={open} setOpen={setOpen}/>
+        </AppBar>
         <Drawer
           variant="permanent"
           anchor="right"
@@ -171,11 +149,10 @@ const MiniDrawer = (props) => {
             {open === true ? <ChevronRight/> : <ChevronLeft/>}
           </IconButton>
         </Drawer>
-        <main className={clsx({
-          [classes.content]: open,
-          [classes.contentUpOpen]: props.contentUp,
-        })}>
-          <div className={classes.toolbar}/>
+        <main
+          className={clsx({
+            [classes.content]: open,
+          })}>
           <Container>
             {props.children}
           </Container>
