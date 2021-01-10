aa57e355a051e24fb1e16804f45c2e468b6d3adf
Vadym <tvadost2003@gmail.com>
2021-01-10 17:32:50 +0200

    header and container

---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({});
+
+const Index = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Index;
