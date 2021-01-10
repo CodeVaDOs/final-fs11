468140455c7920452bd5a4548f66a71fa2a5a556
Вадик Мельниченко <melnicenkovadik@gmail.com>
2021-01-10 20:14:07 +0300

    commit from sidebar

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,16 +1,121 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core";
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import UserBar from "../UserBar";
+import ManagerCard from "../ManagerCard";
+import { ChevronLeft, ChevronRight } from "@material-ui/icons";
 
-const useStyles = makeStyles({});
+const drawerWidth = 440;
 
-const Index = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function Sidebar() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div>
+    <div className={classes.root}>
+      <CssBaseline/>
+      <Drawer
+        variant="permanent"
+        anchor="right"
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}>
+        <Divider/>
 
+        <UserBar
+          name={'Dima Ovsienko'}
+          avatar={'https://bipbap.ru/wp-content/uploads/2017/10/minony-kartinki-smeshnye-na-avatarku.jpg'}
+          notification={8}
+          describe={`Investor`}
+          sms={17}
+        />
+        <Divider/>
+
+        <ManagerCard
+          name={'Oleg Prytula'}
+          tel={'093-111-11-11'}
+          email={'olegprytula@gmail.com'}/>
+        <Divider/>
+
+        <IconButton
+          onClick={handleDrawerToggle}
+          className={classes.menuButton}>
+          {open === true ? <ChevronRight/> : <ChevronLeft/>}
+        </IconButton>
+      </Drawer>
     </div>
   );
-};
-
-export default Index;
+}
---
+++ client/src/components/UserBar/index.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from "@material-ui/core/IconButton";
+import { Badge, Menu, MenuItem } from "@material-ui/core";
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import CheckLanguage from "./../CheckLanguage";
+
+const useStyles = makeStyles((theme) => ({
+  list: {
+    width: "100%",
+    margin: "0 auto",
+    marginTop: "30px"
+  },
+  grow: {
+    flexGrow: 0,
+  },
+  menuButton: {
+    marginRight: theme.spacing(0),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
+    },
+  },
+
+  sectionDesktop: {
+    [theme.breakpoints.up('md')]: {
+      padding: "10px",
+      display: 'flex',
+    },
+  },
+  descrBlock: {
+    display: "flex",
+    alignItems: 'flex-end',
+    flexDirection: 'column',
+    lineHeight: "-0px",
+    position: "relative",
+    marginLeft: '15px'
+  },
+  avatar: {
+    width: '68px',
+    height: '68px',
+    marginLeft: '15px'
+  }
+}));
+
+
+export default function UserBar({ name, describe, sms, notification, avatar }) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [, setMobileMoreAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const menuId = 'primary-search-account-menu';
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'right', horizontal: 'right' }}
+      id={menuId}
+      transformOrigin={{ vertical: 'right', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  return (
+    <React.Fragment>
+      <CssBaseline/>
+      <List className={classes.list}>
+        <React.Fragment>
+          <ListItem>
+            <div className={classes.sectionDesktop}>
+              <CheckLanguage/>
+              <IconButton color="inherit">
+                <Badge badgeContent={sms} color="secondary">
+                  <MailIcon/>
+                </Badge>
+              </IconButton>
+              <IconButton color="inherit">
+                <Badge badgeContent={notification} color="secondary">
+                  <NotificationsIcon/>
+                </Badge>
+              </IconButton>
+            </div>
+            <div className={classes.descrBlock}>
+              <ListItemText primary={name}/><br/>
+              <ListItemText secondary={describe}/>
+            </div>
+            <ListItemAvatar
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+              style={{ cursor: 'pointer' }}>
+              <Avatar className={classes.avatar} alt="Profile Picture" src={avatar}/>
+            </ListItemAvatar>
+          </ListItem>
+        </React.Fragment>
+      </List>
+      {renderMenu}
+
+    </React.Fragment>
+  );
+}
