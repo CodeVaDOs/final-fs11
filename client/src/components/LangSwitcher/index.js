cff91fc2931031a430ea01d21ca79b964c06dc8f
Vadym Tartakovskyi <62440835+CodeVaDOs@users.noreply.github.com>
2021-01-13 21:08:33 +0200

    Panel new user (#52)
    * newUserPanel
    
    * newUserPanel final
    
    * newUserPanel final
    
    * test
    
    Co-authored-by: Nataliia Ivaniv <nat_ivaniv@ukr.net>

---
+++ client/src/components/LangSwitcher/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import { useTranslation } from "react-i18next";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(0),
+    marginLeft: 30,
+    marginTop: -22,
+    minWidth: 10,
+    "@media (min-width:320px)": {
+      marginLeft: 15,
+      marginTop: 10
+    },
+    "@media (min-width:1100px)": {
+      marginLeft: 30,
+      marginTop: -22,
+    },
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const NativeSelects =()=>{
+  const classes = useStyles();
+  const { i18n } = useTranslation();
+  const changeLanguage=(e)=>{
+    i18n.changeLanguage(e.target.value);
+  };
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="uncontrolled-native"></InputLabel>
+        <NativeSelect
+          defaultValue={'ua'}
+          disableUnderline={true}
+          inputProps={{
+            name: 'name',
+            id: 'uncontrolled-native',
+          }}
+          onChange={(e) => {
+            changeLanguage(e);
+          }}
+        >
+          <option value={"en"}>EN</option>
+          <option value={"ru"}>RU</option>
+          <option value={"ua"}>UA</option>
+        </NativeSelect>
+      </FormControl>
+    </div>
+  );
+};
+export default NativeSelects;
