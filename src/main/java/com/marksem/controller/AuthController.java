b8a1a68630a28c51d9c9907758ea4fe49aba44c5
mariiamoroziuk <moroziuk_m@ukr.net>
2021-01-17 21:01:48 +0200

    security refresh token

--- src/main/java/com/marksem/controller/AuthController.java
+++ src/main/java/com/marksem/controller/AuthController.java
@@ -1,58 +1,36 @@
 package com.marksem.controller;
 
 import com.marksem.dto.request.RequestAuth;
-import com.marksem.entity.user.User;
-import com.marksem.repo.UserRepository;
-import com.marksem.security.JwtTokenProvider;
+import com.marksem.dto.request.RequestRefreshToken;
+import com.marksem.service.AuthService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private UserRepository userRepository;
-    private JwtTokenProvider jwtTokenProvider;
-
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticate(@RequestBody RequestAuth request) {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-            User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
-            String token = jwtTokenProvider.createToken(request.getEmail(), user.getRole().name());
-            Map<Object, Object> response = new HashMap<>();
-            response.put("email", request.getEmail());
-            response.put("token", token);
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok(authService.authenticate(request.getEmail(), request.getPassword()));
         } catch (AuthenticationException e) {
             return new ResponseEntity<>("Invalid email/password combination", HttpStatus.FORBIDDEN);
         }
     }
 
-    @PostMapping("/logout")
-    public void logout(HttpServletRequest request, HttpServletResponse response) {
-        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
-        securityContextLogoutHandler.logout(request, response, null);
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestBody RequestRefreshToken request) {
+        try {
+            return ResponseEntity.ok(authService.refresh(request.getRefreshToken()));
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("JWT token is expired or invalid", HttpStatus.UNAUTHORIZED);
+        }
     }
 }
