56c25824e1e617daf7d6a66361510b88d0b51438
Vadym <tvadost2003@gmail.com>
2021-01-17 16:12:19 +0200

    frontend auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "@components/Header";
 import Sidebar from "@components/Sidebar";
 import { makeStyles } from "@material-ui/core";
 import AppContainer from "./containers/AppContainer";
+import { useSelector } from "react-redux";
 
 const sidebarWidth = 416;
 
@@ -15,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
   mainContainer: props => ({
     width: '100%',
     marginLeft: '20px',
-    marginRight: props.isOpenSidebar ? sidebarWidth + 30 + 'px' : '30px',
+    marginRight: props.isOpenSidebar && props.authorized ? sidebarWidth + 30 + 'px' : '30px',
     transition: 'margin 225ms cubic-bezier(0, 0, 0.2, 1) 0ms',
   })
 }));
@@ -23,20 +24,23 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
   const [isOpenSidebar, handleOpenSidebar] = useState(true);
-  const classes = useStyles({ isOpenSidebar });
+  const { authorized } = useSelector(state => state.auth);
+
+  const classes = useStyles({ isOpenSidebar, authorized });
+
 
   return (
     <div className={classes.root}>
       <div className={classes.mainContainer}>
-        <Header/>
+        {authorized && <Header/>}
         <AppContainer/>
       </div>
-      <div>
+      {authorized && <div>
         <Sidebar
           width={sidebarWidth}
           open={isOpenSidebar}
           handleOpen={handleOpenSidebar.bind(null, !isOpenSidebar)}/>
-      </div>
+      </div>}
     </div>
   );
 };
--- client/src/utils/tokens.js
+++ client/src/utils/tokens.js
@@ -1,6 +1,14 @@
 export const getTokens = () => {
   return {
-    accessToken: localStorage.getItem("auth_token"),
+    accessToken: localStorage.getItem("authToken"),
     refreshToken: localStorage.getItem("refreshToken"),
   };
+};
+
+export const setAuthToken = (token) => {
+  localStorage.setItem("authToken", token);
+};
+
+export const setRefreshToken = (token) => {
+  localStorage.setItem("refreshToken", token);
 };
