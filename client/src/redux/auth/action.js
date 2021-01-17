8061b5267a37e2abecfa5d8a74bd1f92b357dbfa
Vadym Tartakovskyi <62440835+CodeVaDOs@users.noreply.github.com>
2021-01-17 16:13:02 +0200

    Auth (#70)
    * entities 1.0
    
    * entities 1.1
    
    * entities 1.2
    
    * cleanup code
    
    * pom.xml
    
    * entities 1.3
    
    * security
    
    * fix
    
    * fix
    
    * security 1.1
    
    * frontend auth
    
    Co-authored-by: mariiamoroziuk <moroziuk_m@ukr.net>

---
+++ client/src/redux/auth/action.js
@@ -0,0 +1,58 @@
+import api from "@utils/api";
+import { catchError, getTokens, setAuthToken, setRefreshToken } from "../../utils";
+
+
+
+const logOut = () => (dispatch) => {
+  api.get('auth/logout')
+    .then(() => {
+      console.log("success logout");
+    });
+  setAuthToken();
+  setRefreshToken();
+  dispatch({ type: "LOGOUT" });
+};
+
+const logIn = (values) => (dispatch) => {
+  dispatch({ type: "LOGIN_REQUEST" });
+
+  console.log(values);
+
+  api
+    .post('auth/login', values)
+    .then((data) => {
+      console.log("success log in");
+      setAuthToken(data.jwt);
+      setRefreshToken(data.refreshToken);
+      dispatch({ type: "LOGIN_SUCCESS", payload: data.user });
+    })
+    .catch((err) => {
+      catchError(err);
+      dispatch({ type: "LOGIN_FAILURE" });
+    });
+};
+
+// const fetchProfile = () => (dispatch) => {
+//   const { accessToken } = getTokens();
+//   dispatch(ACTIONS.profile.request());
+//
+//   api
+//     .get(URLS.USER.PROFILE, {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     })
+//     .then((data) => {
+//       successToastMessage("Welcome back!");
+//       dispatch(ACTIONS.login.success(data));
+//     })
+//     .catch((err) => {
+//       catchError(err);
+//       dispatch(performLogout());
+//     });
+// };
+
+export const API_ACTIONS = {
+  logIn,
+  logOut,
+};
