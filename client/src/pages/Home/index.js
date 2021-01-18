9a359c00a07ea698a1a621da60098f618fc27cac
Nataliia Ivaniv <nat_ivaniv@ukr.net>
2021-01-18 15:52:56 +0200

    FastAccessPanel

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import News from "../../components/News";
 import { Button } from "@material-ui/core";
-
+import FastAccessPanel from "../../components/FastAccessPanel";
 const Home = () => {
   const { t } = useTranslation();
   const [hasError, setHasError] = useState(false);
@@ -13,10 +13,7 @@ const Home = () => {
   }
   return (
     <div>
-      <h1>{t('home')} </h1>
       <Button onClick={onClick}>Error</Button>
-
-      <News/>
     </div>
   );
 
