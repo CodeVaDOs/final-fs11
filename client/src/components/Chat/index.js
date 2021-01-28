84b92003e474e972e7106235c34aad989be8779d
melnicenkovadik <melnicenkovadik@gmail.com>
2021-01-28 17:03:35 +0200

    remove dependence

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Chat from "./Chat";
 
-import "dayjs/locale/ru";
 import "./css/index.css";
 
 export const UserChat = () => {
--- client/src/components/DatePicker/index.js
+++ client/src/components/DatePicker/index.js
@@ -1,12 +1,10 @@
 // import 'date-fns';
-import React, { useState }from 'react';
-import DateFnsUtils from '@date-io/date-fns';
+import React, { useState } from 'react';
 import {
-  MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 
-const MaterialUIPickers=()=> {
+const MaterialUIPickers = () => {
   // The first commit of Material-UI
   const [selectedDate, setSelectedDate] = useState(new Date('2021-01-18T21:11:54'));
   const handleDateChange = (date) => {
@@ -14,21 +12,19 @@ const MaterialUIPickers=()=> {
   };
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <KeyboardDatePicker
-        style={{ border: "0.5px solid #B1B4BA", marginLeft: "5px", borderRadius:"5px", height:"21px", padding:10 }}
-        disableToolbar
-        variant="outlined"
-        format="MM/dd/yyyy"
-        margin="normal"
-        id="date-picker-inline"
-        value={selectedDate}
-        onChange={handleDateChange}
-        KeyboardButtonProps={{
-          'aria-label': 'change date',
-        }}
-      />
-    </MuiPickersUtilsProvider>
+    <KeyboardDatePicker
+      style={{ border: "0.5px solid #B1B4BA", marginLeft: "5px", borderRadius: "5px", height: "21px", padding: 10 }}
+      disableToolbar
+      variant="outlined"
+      format="MM/dd/yyyy"
+      margin="normal"
+      id="date-picker-inline"
+      value={selectedDate}
+      onChange={handleDateChange}
+      KeyboardButtonProps={{
+        'aria-label': 'change date',
+      }}
+    />
   );
 };
 export default MaterialUIPickers;
