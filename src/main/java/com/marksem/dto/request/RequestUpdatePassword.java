44beddbacd7c143865e504fa7a4521ef0c0a4511
Vadym Tartakovskyi <62440835+CodeVaDOs@users.noreply.github.com>
2021-01-29 19:20:58 +0200

    Errors&validation (#102)
    * security refresh exception
    
    * validation, exception 1.0
    
    Co-authored-by: mariiamoroziuk <moroziuk_m@ukr.net>

--- src/main/java/com/marksem/dto/request/RequestUpdatePassword.java
+++ src/main/java/com/marksem/dto/request/RequestUpdatePassword.java
@@ -2,7 +2,10 @@ package com.marksem.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.Size;
+
 @Data
 public class RequestUpdatePassword {
+    @Size(min = 4, message = "password is too short")
     private String password;
 }
--- src/main/java/com/marksem/exception/ControllerAdvisor.java
+++ src/main/java/com/marksem/exception/ControllerAdvisor.java
@@ -1,18 +1,22 @@
 package com.marksem.exception;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import com.marksem.dto.response.ResponseException;
+import lombok.NonNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
@@ -21,41 +25,46 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleJwtAuthenticationException(
             JwtAuthenticationException ex, WebRequest request) {
 
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", ex.getMessage());
-
-        return new ResponseEntity<>(body, ex.getStatus());
+        return ResponseEntity
+                .status(ex.getStatus())
+                .body(new ResponseException(ex.getMessage()));
     }
 
     @ExceptionHandler(NoDataFoundException.class)
     public ResponseEntity<Object> handleNoDataFoundException(
             NoDataFoundException ex, WebRequest request) {
 
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ResponseException(ex.getMessage()));
+    }
 
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ResponseException> handleElementNotFound(NoSuchElementException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ResponseException(ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ResponseException> handleParamNotValid(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ResponseException(ex.getMessage()));
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, HttpHeaders headers,
-            HttpStatus status, WebRequest request) {
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
 
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", status.value());
-
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
-
-        body.put("errors", errors);
-
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ResponseException(errors.values().toString()));
     }
 }
