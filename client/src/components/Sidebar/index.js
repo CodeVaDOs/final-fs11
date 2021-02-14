d1c23c55d4a2d29670b0c7df960187dc202fb74b
Nataliia Ivaniv <nat_ivaniv@ukr.net>
2021-02-14 10:41:53 +0200

    SideBarTasks

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -8,22 +8,13 @@ import Box from '@material-ui/core/Box';
 import Typography from "@material-ui/core/Typography";
 import "react-datepicker/dist/react-datepicker.css";
 import Calendar from '../../components/Calendar';
-
+import { useTranslation } from "react-i18next";
+import TaskPanel from '../../components/TasksPanel';
 const useStyles = makeStyles(theme => ({
-  header: {
-    fontFamily: 'Roboto, sans-serif',
-    fontSize: '18px',
-    fontWeight: 'bold',
-    color: '#254A93',
-    textAlign: "center",
-    marginTop: "20px"
-
-  },
   openHandler: props => ({
     position: 'absolute',
     top: '50px',
     right: props.open ? '418' - 39 / 2 + 'px' : '-' + 39 / 2 + 'px',
-
     transition: 'right 225ms cubic-bezier(0, 0, 0.2, 1) 0ms',
     border: '1px solid #b1b4ba',
     backgroundColor: '#eef5ff',
@@ -35,9 +26,16 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
     cursor: 'pointer',
     outline: 'none',
-
     zIndex: 12021
   }),
+  header: {
+    fontFamily: 'Roboto, sans-serif',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    color: '#254A93',
+    textAlign: "center",
+    marginTop: "20px"
+  },
 
   collapseButtonImage: props => ({
     transform: props.open ? 'rotate(90deg)' : 'rotate(90deg) rotateX(180deg)',
@@ -61,20 +59,26 @@ const useStyles = makeStyles(theme => ({
   }),
 }));
 
-const Index = ({ width, open, handleOpen }) => {
+const Index = ( { width, open, handleOpen } )=> {
+  const { t } = useTranslation();
   const classes = useStyles({ width, open });
   return (
     <>
       <button className={classes.openHandler} onClick={handleOpen}>
         <img className={classes.collapseButtonImage} src={buttonArrow} alt="button arrow"/>
       </button>
-
       <Drawer classes={{ paper: classes.drawer }} variant="persistent" open={open} anchor="right">
         <SidebarMenuItem/>
-        <FastAccessPanel/>
-        <UserChat/>
         <Box>
-          <Typography className={classes.header}>{"Календар бронювання"}</Typography>
+          <Typography className={classes.header}>{t("fastAccess")}</Typography>
+          <FastAccessPanel/>
+        </Box>
+        <TaskPanel/>
+        <Box>
+          <UserChat/>
+        </Box>
+        <Box>
+          <Typography className={classes.header}>{t("calendarB")}</Typography>
           <Calendar/>
         </Box>
       </Drawer>
--- client/src/redux/auth/reducer.js
+++ client/src/redux/auth/reducer.js
@@ -3,7 +3,7 @@ import { getTokens } from "../../utils";
 const { accessToken } = getTokens();
 // authorized: Boolean(accessToken),
 const INIT_STATE = {
-  authorized: false,
+  authorized: Boolean(accessToken),
   loading: false,
   user: {},
 };
