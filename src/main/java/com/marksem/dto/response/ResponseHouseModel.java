77b812513446b4517e35b2f0f35915d8fabde59a
Vadym <tvadost2003@gmail.com>
2021-03-06 12:23:07 +0200

    rever

--- src/main/java/com/marksem/dto/response/ResponseHouseModel.java
+++ src/main/java/com/marksem/dto/response/ResponseHouseModel.java
@@ -7,12 +7,10 @@ import lombok.Data;
 
 @Data
 public class ResponseHouseModel {
-    private Long id;
     private String name;
     private String houseType;
 
     public ResponseHouseModel(HouseModel hm) {
-        this.id = hm.getId();
         this.name = hm.getName();
         this.houseType = hm.getHouseType().getName();
     }
