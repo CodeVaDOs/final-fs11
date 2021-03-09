3b1ad2e017603e77a30f983c49461c24ebede052
mariiamoroziuk <54170630+mariiamoroziuk@users.noreply.github.com>
2021-03-09 20:17:31 +0200

    Data to front3 (#191)
    * dataToFront3 1.0
    
    * dataToFront3 1.1
    
    * dataToFront3 1.2
    
    * dataToFront3 1.3
    
    * dataToFront3 1.4
    
    * dataToFront3 1.5
    
    * dataToFront3 1.5
    
    * dataToFront3 1.6
    
    * houses
    
    * contacts
    
    * changePassword
    
    * contacts
    
    Co-authored-by: Vadym Tartakovskyi <62440835+CodeVaDOs@users.noreply.github.com>

--- src/main/java/com/marksem/dto/response/ResponseHouseImage.java
+++ src/main/java/com/marksem/dto/response/ResponseHouseImage.java
@@ -1,7 +1,6 @@
 package com.marksem.dto.response;
 
 import com.marksem.entity.house.HouseImage;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -9,11 +8,9 @@ import lombok.EqualsAndHashCode;
 @Data
 public class ResponseHouseImage extends BaseEntity {
     private String url;
-    private Long houseId;
 
     public ResponseHouseImage(HouseImage hi) {
         super(hi);
         this.url = hi.getUrl();
-        this.houseId = hi.getHouse().getId();
     }
 }
