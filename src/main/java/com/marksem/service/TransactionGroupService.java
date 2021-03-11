89b1a8510b1c41a685248fe7367b08a132776d70
unknown <eduardyermolenkowork@gmail.com>
2021-03-11 21:50:26 +0200

    end fix currency service

--- src/main/java/com/marksem/service/TransactionGroupService.java
+++ src/main/java/com/marksem/service/TransactionGroupService.java
@@ -3,6 +3,7 @@ package com.marksem.service;
 import com.marksem.dto.request.RequestTransactionGroup;
 import com.marksem.dto.response.ResponseTransactionGroup;
 import com.marksem.entity.transaction.Currency;
+import com.marksem.entity.transaction.FinanceType;
 import com.marksem.entity.transaction.TransactionGroup;
 import com.marksem.exception.NoDataFoundException;
 import com.marksem.repository.HouseRepository;
@@ -28,9 +29,8 @@ public class TransactionGroupService {
                     houseRepository
                       .findById(requestTransactionGroup.getHouseId())
                       .map(h -> {
-                        Double convert = currencyConversionService.convert(requestTransactionGroup.getAmount(),
-                          requestTransactionGroup.getCurrency(),
-                          Currency.USD);
+                        Double convert = convertAmount(requestTransactionGroup.getAmount(),
+                            requestTransactionGroup.getCurrency(), requestTransactionGroup.getTransactionType());
                         TransactionGroup transactionGroupSave = requestTransactionGroup.toEntity(h, convert);
                         transactionGroupSave.setTransactions(transactionService.create(transactionGroupSave));
                         return transactionGroupRepository.save(transactionGroupSave);
@@ -41,6 +41,12 @@ public class TransactionGroupService {
              .collect(Collectors.toList());
   }
 
+
+  private Double convertAmount(Double amount, Currency currency, FinanceType transactionType) {
+    return transactionType.equals(FinanceType.INCOME) ?
+        currencyConversionService.convert(amount, currency, Currency.USD)
+        : -1 * currencyConversionService.convert(amount, currency, Currency.USD);
+  };
 
 //    public ResponseTransactionGroup create(RequestTransactionGroup tg) {
 //        return houseRepository.findById(tg.getHouseId())
