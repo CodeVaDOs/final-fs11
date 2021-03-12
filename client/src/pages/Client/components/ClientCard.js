ec48d4f3969ff6763d291895c520effa540e9c2f
VaDOs <tvadost2003@gmail.com>
2021-03-12 08:41:36 +0200

    employees

--- client/src/pages/Client/components/ClientCard.js
+++ client/src/pages/Client/components/ClientCard.js
@@ -4,6 +4,8 @@ import {makeStyles} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import {ChatRounded} from "@material-ui/icons";
 import {Link} from "react-router-dom";
+import ClientCardProfile from "./ClientCardProfile";
+import defaultUser from "../../../images/avatar.png";
 
 const useStyles = makeStyles({
     root: {
@@ -17,13 +19,7 @@ const useStyles = makeStyles({
         alignItems: 'center'
     },
 
-    profile: {
-      width: '42%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignContent: 'space-between',
-        padding: 0
-    },
+
 
     divider: {
       background: '#acb5b9',
@@ -61,100 +57,13 @@ const useStyles = makeStyles({
         verticalAlign: 'middle'
     },
 
-    userLink: {
-      display: 'flex',
-      alignItems: 'flex-end',
-    },
 
-    userAvatar: {
-        borderRadius: '50%',
-        objectFit: 'cover',
-        width: '40px',
-        height: '40px',
-        marginRight: '15px'
-    },
-
-    userName: {
-        fontWeight: 500,
-        fontSize: '16px',
-        letterSpacing: '0em',
-        color: '#293134'
-    },
 
     buttonContainer: {
         marginTop: '21px'
     },
-
-    userData: {
-        marginTop: '5px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-
-    leftSideContent: {
-        width: '45%',
-        paddingRight: '15px',
-        flexDirection: 'column',
-    },
-
-    rightSideContent: {
-        display: 'flex',
-        width: '55%',
-        paddingLeft: '15px',
-        flexDirection: 'column',
-
-    },
-
-    phoneNumber: {
-        fontSize: '14px',
-        color: '#6e7375',
-        letterSpacing: '0em'
-    },
-
-    email: {
-        fontSize: '14px',
-        color: '#254a93',
-        letterSpacing: '0em'
-    },
-
-    statusLabel: {
-        fontWeight: 500,
-        fontSize: '16px',
-        letterSpacing: '0em',
-        color: '#293134',
-        margin: '10px 0 0'
-    },
-    statusValue: {
-        color: '#6e7375',
-        fontSize: '14px',
-        letterSpacing: '0em',
-        margin: '5px 0 0 0'
-    },
-
-    link: {
-        textDecoration: 'none'
-    }
 })
 
-// birthday: "1988-02-28T08:31:37.417+00:00"
-// contacts: []
-// createDate: "2021-03-02T08:38:07.868+00:00"
-// createdBy: "test@gmail.com"
-// currency: "EUR"
-// email: "testUser@ukr.net"
-// id: 9
-// language: "UKRAINIAN"
-// manager: null
-// managerId: 3
-// name: "Тест Юзер"
-// notifications: []
-// role: "MANAGER"
-// tasks: []
-// updateDate: "2021-03-02T08:38:07.868+00:00"
-// updatedBy: "test@gmail.com"
-// urlAvatar: "http://upruda.ru/images/business-portraits/003.jpg"
-// version: 0
 
 const ClientCard = ({user}) => {
     const classes = useStyles();
@@ -164,35 +73,8 @@ const ClientCard = ({user}) => {
     const phone = contacts.find(contact => contact.type === "MAIN")?.phone || '+380933232222';
 
     return <Card className={classes.root}>
-        <CardContent className={classes.profile}>
-            <div className={classes.userLink}>
-                <Link className={classes.link} to={"clients/" + id}>
-                    <img src={urlAvatar} alt="user avatar" className={classes.userAvatar}/>
-                    <Typography variant="h3" className={classes.userName}>
-                        {name}
-                    </Typography>
-                </Link>
-            </div>
-            <div className={classes.userData}>
-                <div className={classes.leftSideContent}>
-                    <Typography className={classes.phoneNumber}>{phone}</Typography>
-                    <Typography>
-                        <h4 className={classes.statusLabel}>Статус</h4>
-                        <p className={classes.statusValue}>Постыйний клыент</p>
-                    </Typography>
-                </div>
 
-                <Divider className={classes.divider} orientation="vertical" flexItem/>
-
-                <div className={classes.rightSideContent}>
-                    <Typography className={classes.email}>{email}</Typography>
-                    <Typography>
-                        <h4 className={classes.statusLabel}>Будинкыв: 2</h4>
-                        <p className={classes.statusValue}>Постыйний клыент</p>
-                    </Typography>
-                </div>
-            </div>
-        </CardContent>
+        <ClientCardProfile email={email} name={name} phone={phone} urlAvatar={urlAvatar || defaultUser} id={id}/>
 
         <Divider className={classes.divider} orientation="vertical" flexItem/>
 
