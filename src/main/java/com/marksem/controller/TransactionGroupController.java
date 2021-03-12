f761b9cd37bde4d32ddf433578882970d14436e6
mariiamoroziuk <moroziuk_m@ukr.net>
2021-03-12 12:10:07 +0200

    stat 1.1

--- src/main/java/com/marksem/controller/TransactionGroupController.java
+++ src/main/java/com/marksem/controller/TransactionGroupController.java
@@ -22,22 +22,16 @@ public class TransactionGroupController {
         return ResponseEntity.ok(service.create(tg));
     }
 
-//    @PostMapping
-//    @PreAuthorize("hasAuthority('developers:write')")
-//    public ResponseEntity<ResponseTransactionGroup> create(@RequestBody RequestTransactionGroup tg) {
-//        return ResponseEntity.ok(service.create(tg));
-//    }
-//
-//    @PutMapping
-//    @PreAuthorize("hasAuthority('developers:write')")
-//    public ResponseEntity<ResponseTransactionGroup> update(@RequestBody RequestTransactionGroup tg) {
-//        return ResponseEntity.ok(service.update(tg));
-//    }
-//
-//    @DeleteMapping("{id}")
-//    @PreAuthorize("hasAuthority('developers:write')")
-//    public ResponseEntity<Long> delete(@PathVariable("id") Long id) {
-//        return ResponseEntity.ok(service.delete(id));
-//    }
+    @GetMapping
+    @PreAuthorize("hasAuthority('developers:read')")
+    public ResponseEntity<PageableResponse<ResponseTransactionGroup>> readAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(service.readAll(page, size));
+    }
+
+    @GetMapping("{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
+    public ResponseEntity<ResponseTransactionGroup> read(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(service.read(id));
+    }
 
 }
--- src/main/java/com/marksem/service/TransactionGroupService.java
+++ src/main/java/com/marksem/service/TransactionGroupService.java
@@ -1,6 +1,7 @@
 package com.marksem.service;
 
 import com.marksem.dto.request.RequestTransactionGroup;
+import com.marksem.dto.response.PageableResponse;
 import com.marksem.dto.response.ResponseTransactionGroup;
 import com.marksem.entity.transaction.Currency;
 import com.marksem.entity.transaction.TransactionGroup;
@@ -8,6 +9,8 @@ import com.marksem.exception.NoDataFoundException;
 import com.marksem.repository.HouseRepository;
 import com.marksem.repository.TransactionGroupRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,45 +19,41 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class TransactionGroupService {
-  private final HouseRepository houseRepository;
-  private final TransactionGroupRepository transactionGroupRepository;
-  private final CurrencyConversionService currencyConversionService;
-  private final TransactionService transactionService;
+    private final HouseRepository houseRepository;
+    private final TransactionGroupRepository transactionGroupRepository;
+    private final CurrencyConversionService currencyConversionService;
+    private final TransactionService transactionService;
 
-  public List<ResponseTransactionGroup> create(List<RequestTransactionGroup> tg) {
+    public List<ResponseTransactionGroup> create(List<RequestTransactionGroup> tg) {
 
-    return tg.parallelStream()
-             .map(requestTransactionGroup ->
-                    houseRepository
-                      .findById(requestTransactionGroup.getHouseId())
-                      .map(h -> {
-                        Double convert = currencyConversionService.convert(requestTransactionGroup.getAmount(),
-                          requestTransactionGroup.getCurrency(),
-                          Currency.USD);
-                        TransactionGroup transactionGroupSave = requestTransactionGroup.toEntity(h, convert);
-                        transactionGroupSave.setTransactions(transactionService.create(transactionGroupSave));
-                        return transactionGroupRepository.save(transactionGroupSave);
+        return tg.parallelStream()
+                .map(requestTransactionGroup ->
+                        houseRepository
+                                .findById(requestTransactionGroup.getHouseId())
+                                .map(h -> {
+                                    Double convert = currencyConversionService.convert(requestTransactionGroup.getAmount(),
+                                            requestTransactionGroup.getCurrency(),
+                                            Currency.USD);
+                                    TransactionGroup transactionGroupSave = requestTransactionGroup.toEntity(h, convert);
+                                    transactionGroupSave.setTransactions(transactionService.create(transactionGroupSave));
+                                    return transactionGroupRepository.save(transactionGroupSave);
 
-                      })
-                      .map(ResponseTransactionGroup::new)
-                      .orElseThrow(() -> new NoDataFoundException("house", requestTransactionGroup.getHouseId())))
-             .collect(Collectors.toList());
-  }
+                                })
+                                .map(ResponseTransactionGroup::new)
+                                .orElseThrow(() -> new NoDataFoundException("house", requestTransactionGroup.getHouseId())))
+                .collect(Collectors.toList());
+    }
 
-//    public ResponseTransactionGroup update(RequestTransactionGroup tg) {
-//        return transactionGroupRepository.findById(tg.getId())
-//                .map(i -> {
-//                    i.setFromDate(tg.getFromDate());
-//                    i.setToDate(tg.getToDate());
-//                    return transactionGroupRepository.save(i);
-//                })
-//                .map(ResponseTransactionGroup::new)
-//                .orElseThrow(() -> new NoDataFoundException("transaction group", tg.getId()));
-//    }
-//
-//    public Long delete(Long id) {
-//        transactionGroupRepository.deleteById(id);
-//        return id;
-//    }
+    public ResponseTransactionGroup read(Long id) {
+        return transactionGroupRepository.findById(id)
+                .map(ResponseTransactionGroup::new)
+                .orElseThrow(() -> new NoDataFoundException("transaction group", id));
+    }
+
+    public PageableResponse<ResponseTransactionGroup> readAll(int page, int size) {
+        Page<TransactionGroup> groups = transactionGroupRepository.findAll(PageRequest.of(page, size));
+        return new PageableResponse<>(groups.getTotalElements(),
+                groups.getContent().stream().map(ResponseTransactionGroup::new).collect(Collectors.toList()));
+    }
 }
 
